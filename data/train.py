6758a027e942ecb966dc63ebf575080029089e93
Felipe González Urrego <felipe57@utp.edu.co>
2023-10-25 21:32:21 -0500

    Delete data/train.py

--- data/train.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-from sklearn import preprocessing
-from sklearn.model_selection import StratifiedKFold
-from sklearn.metrics import roc_auc_score, roc_curve
-from xgboost import XGBClassifier
-
-# parameters
